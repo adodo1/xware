c105f470a087816fd98dba4c21fa4958792852f1
adodo1 <adodo1@126.com>
2017-03-11 09:30:02 +0800

    麻烦 构建一次3分钟

--- runcommand.sh
+++ runcommand.sh
@@ -1,5 +1,5 @@
 ﻿#!/usr/bin/env bash
-sed 's/\/var\/www\/html;/\/data;\n        autoindex on;\n        autoindex_exact_size off;\n        autoindex_localtime on;\n        location ~ \\.php\$ {\n               include snippets/fastcgi-php.conf;\n               fastcgi_pass unix:/run/php/php7.0-fpm.sock;\n        }\n/' /etc/nginx/sites-enabled/default -i
+sed 's/\/var\/www\/html;/\/data;\n        autoindex on;\n        autoindex_exact_size off;\n        autoindex_localtime on;\n        location ~ \\.php\$ {\n               include snippets\/fastcgi-php.conf;\n               fastcgi_pass unix:\/run\/php\/php7.0-fpm.sock;\n        }\n/' /etc/nginx/sites-enabled/default -i
 /etc/init.d/nginx restart
 
 # 添加探针文件
